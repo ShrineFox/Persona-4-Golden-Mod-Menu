4b5c51541cb555678ae97e82325b4ffaa86ca1f9
Ryan ShrineFox <ShrineFox@users.noreply.github.com>
2020-11-09 23:01:55 -0500

    Initial Commit
    Created to Sierra, just cleaned up to match P5 mod menu

---
+++ build.bat
@@ -0,0 +1,18 @@
+set PAKPACK="path to PAKPack.exe"
+set COMPILER="path to AtlusScriptCompiler.exe"
+set BUILD_PATH=.\build
+set INPUT_PATH=%BUILD_PATH%\input
+set OUTPUT_PATH=%BUILD_PATH%\output
+
+if not exist %BUILD_PATH% mkdir %BUILD_PATH%
+if not exist %INPUT_PATH% mkdir %INPUT_PATH%
+if not exist %OUTPUT_PATH% mkdir %OUTPUT_PATH%
+if not exist %OUTPUT_PATH%\field mkdir %OUTPUT_PATH%\field
+
+%PAKPACK% unpack "%INPUT_PATH%\init_free.bin"   %INPUT_PATH%\extracted
+
+%COMPILER% .\field\field.bf.flow -Compile -OutFormat V1 -Library P4G -Encoding P4 -Out "%OUTPUT_PATH%\field\field.bf" -Hook
+
+%PAKPACK% replace "%INPUT_PATH%\init_free.bin"   field/script/field.bf   "%OUTPUT_PATH%\field\field.bf"   "%OUTPUT_PATH%\init_free.bin"
+
+del "%OUTPUT_PATH%\field\field.bf"
