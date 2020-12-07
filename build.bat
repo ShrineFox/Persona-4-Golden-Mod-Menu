1db05fd57d6f6638f5bee861f7080cf9e01c7256
TekkaGB <tekka.gb@gmail.com>
2020-12-07 08:39:49 -0800

    Organized flow files into folders
    Hooked Find a Friend and MobileCalendar within quick travel menu
    
    Removed page and exit buttons in mod menu in favor of hidden button shortcuts
    
    Fixed and adjusted Quick Travel Plus
    
    Added a few more functions in Mod Menu

--- build.bat
+++ build.bat
@@ -8,17 +8,29 @@ if not exist %BUILD_PATH% mkdir %BUILD_PATH%
 if not exist %INPUT_PATH% mkdir %INPUT_PATH%
 if not exist %OUTPUT_PATH% mkdir %OUTPUT_PATH%
 if not exist %OUTPUT_PATH%\field mkdir %OUTPUT_PATH%\field
+if not exist %OUTPUT_PATH%\field\pack mkdir %OUTPUT_PATH%\field\pack
 
-%PAKPACK% unpack "%INPUT_PATH%\init_free.bin"   %INPUT_PATH%\extracted
+%PAKPACK% unpack "%INPUT_PATH%\init_free.bin"
+%PAKPACK% unpack "%INPUT_PATH%\field\pack\fd007_001.arc"
+%PAKPACK% unpack "%INPUT_PATH%\field\pack\fd010_002.arc"
 
-%COMPILER% .\field\field.bf.flow -Compile -OutFormat V1 -Library P4G -Encoding P4 -Out "%OUTPUT_PATH%\field\field.bf" -Hook
-%COMPILER% .\dungeon\dungeon.bf.flow -Compile -OutFormat V1 -Library P4G -Encoding P4 -Out "%OUTPUT_PATH%\field\script\dungeon.bf" -Hook
+%COMPILER% .\init_free\field.bf.flow -Compile -OutFormat V1 -Library P4G -Encoding P4 -Out "%OUTPUT_PATH%\field.bf" -Hook
+%COMPILER% .\field\dungeon.bf.flow -Compile -OutFormat V1 -Library P4G -Encoding P4 -Out "%OUTPUT_PATH%\field\script\dungeon.bf" -Hook
+%COMPILER% .\field\f007.bf.flow -Compile -OutFormat V1 -Library P4G -Encoding P4 -Out "%OUTPUT_PATH%\field\pack\fd007_001\f007.bf" -Hook
+%COMPILER% .\field\f010.bf.flow -Compile -OutFormat V1 -Library P4G -Encoding P4 -Out "%OUTPUT_PATH%\field\pack\fd010_002\f010.bf" -Hook
 
-%PAKPACK% replace "%INPUT_PATH%\init_free.bin"   field/script/field.bf   "%OUTPUT_PATH%\field\field.bf"   "%OUTPUT_PATH%\init_free.bin"
+%PAKPACK% replace "%INPUT_PATH%\init_free.bin"   field/script/field.bf   "%OUTPUT_PATH%\field.bf"   "%OUTPUT_PATH%\init_free.bin"
+%PAKPACK% replace "%INPUT_PATH%\field\pack\fd007_001.arc"   f007.bf   "%OUTPUT_PATH%\field\pack\fd007_001\f007.bf"   "%OUTPUT_PATH%\field\pack\fd007_001.arc"
+%PAKPACK% replace "%INPUT_PATH%\field\pack\fd010_002.arc"   f010.bf   "%OUTPUT_PATH%\field\pack\fd010_002\f010.bf"   "%OUTPUT_PATH%\field\pack\fd010_002.arc"
 
-del /s /q "%INPUT_PATH%\extracted\*"
-rmdir /s /q "%INPUT_PATH%\extracted"
-del /s /q "%OUTPUT_PATH%\field\*"
-rmdir /s /q "%OUTPUT_PATH%\field"
-del "%OUTPUT_PATH%\field\field.bf"
-del "%OUTPUT_PATH%\field\script\dungeon.bf"
+del /s /q "%INPUT_PATH%\init_free\*"
+rmdir /s /q "%INPUT_PATH%\init_free"
+del /s /q "%INPUT_PATH%\field\pack\fd007_001\*"
+rmdir /s /q "%INPUT_PATH%\field\pack\fd007_001"
+del /s /q "%INPUT_PATH%\field\pack\fd010_002\*"
+rmdir /s /q "%INPUT_PATH%\field\pack\fd010_002"
+del /s /q "%OUTPUT_PATH%\field\pack\fd007_001\*"
+rmdir /s /q "%OUTPUT_PATH%\field\pack\fd007_001"
+del /s /q "%OUTPUT_PATH%\field\pack\fd010_002\*"
+rmdir /s /q "%OUTPUT_PATH%\field\pack\fd010_002"
+del "%OUTPUT_PATH%\field.bf"
